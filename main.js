29a912485289cbe503b36cfaef2d42b04f4329e5
itobuka <itobuka2019-10@ftn.edu.rs>
2020-08-23 22:18:05 +0200

    separating tables

--- main.js
+++ main.js
@@ -5,25 +5,121 @@ let nizOpcija = [
     id: 1,
     vrstaJela: "Caj",
     jedinicaMere: "L",
-    /* kolicinaJela: {
+    kolicinaJela: {
       poVojniku: 0.3,
       svega: 6.9
-    }, */
+    },
+    /* kolicinaJelaPoVojniku: 0.3,
+    kolicinaJelaSvega: 6.9, */
+    termickaSredstvaIPosudjeZaPripremuJela: "1 x 10L",
+    vremeObade: "05.00 - 05.30",
+    osobljeZaTermickuObradu: "Radomirovic",
+    linija1: {
+      kolicinaJela: "300g",
+      osobljeZaPodelu: "Sneki"
+    },
+    /* linija1Kolicina: "330g",
+    linija1Osoblje: "Sneki", */
+    linija2: {
+      kolicinaJela: "500g",
+      osobljeZaPodelu: "Mile"
+    }
+    /* linija2Kolicina: "500g",
+    linija2Osoblje: "Keba" */
+  },
+  {
+    id: 2,
+    vrstaJela: "Sunkarica",
+    jedinicaMere: "kg",
+    kolicinaJela: {
+      poVojniku: 0.1,
+      svega: 6.9
+    },
+    /* kolicinaJelaPoVojniku: 0.1,
+    kolicinaJelaSvega: 6.9, */
+    termickaSredstvaIPosudjeZaPripremuJela: "noz",
+    vremeObade: "05.10 - 05.30",
+    osobljeZaTermickuObradu: "Radomirovic",
+    linija1: {
+      kolicinaJela: "300g",
+      osobljeZaPodelu: "Super Sneki"
+    },
+    /* linija1Kolicina: "300g",
+    linija1Osoblje: "Ceki", */
+    linija2: {
+      kolicinaJela: "500g",
+      osobljeZaPodelu: "Mile Kitic"
+    }
+    /* linija2Kolicina: "500g",
+    linija2Osoblje: "Mile" */
+  },
+  {
+    id: 3,
+    vrstaJela: "Sir topljeni",
+    jedinicaMere: "kg",
+    kolicinaJela: {
+      poVojniku: 0.05,
+      svega: 1.15
+    },
+    /* kolicinaJelaPoVojniku: 0.05,
+    kolicinaJelaSvega: 1.15, */
+    termickaSredstvaIPosudjeZaPripremuJela: " / ",
+    vremeObade: "05:30 - 05:40",
+    osobljeZaTermickuObradu: "Djuric Nenad",
+    linija1: {
+      kolicinaJela: "300g",
+      osobljeZaPodelu: "Viki"
+    },
+    /* linija1Kolicina: "560g",
+    linija1Osoblje: "Viki", */
+    linija2: {
+      kolicinaJela: "500g",
+      osobljeZaPodelu: "Tuki"
+    }
+    /* linija2Kolicina: "500g",
+    linija2Osoblje: "Tuki" */
+  },
+  {
+    id: 4,
+    vrstaJela: "Jaje kuvano",
+    jedinicaMere: "Kom",
+    kolicinaJela: {
+      poVojniku: 1,
+      svega: 23
+    },
+    /* kolicinaJelaPoVojniku: 1,
+    kolicinaJelaSvega: 23, */
+    termickaSredstvaIPosudjeZaPripremuJela: "1 x 10L",
+    vremeObade: "05.30 - 05.50",
+    osobljeZaTermickuObradu: "Radomirovic",
+    linija1: {
+      kolicinaJela: "300g",
+      osobljeZaPodelu: "Rasta"
+    },
+    /* linija1Kolicina: "50g",
+    linija1Osoblje: "Sani", */
+    linija2: {
+      kolicinaJela: "500g",
+      osobljeZaPodelu: "Krasta"
+    }
+    /* linija2Kolicina: "150g",
+    linija2Osoblje: "Baja" */
+  }
+]
+
+// data ver2
+let nizOpcijaKrace = [
+  {
+    id: 1,
+    vrstaJela: "Caj",
+    jedinicaMere: "L",
     kolicinaJelaPoVojniku: 0.3,
     kolicinaJelaSvega: 6.9,
     termickaSredstvaIPosudjeZaPripremuJela: "1 x 10L",
     vremeObade: "05.00 - 05.30",
     osobljeZaTermickuObradu: "Radomirovic",
-    /* linija1: {
-      kolicinaJela: "300g",
-      osobljeZaPodelu: "Sneki"
-    }, */
     linija1Kolicina: "330g",
     linija1Osoblje: "Sneki",
-    /* linija2: {
-      kolicinaJela: "500g",
-      osobljeZaPodelu: "Mile"
-    } */
     linija2Kolicina: "500g",
     linija2Osoblje: "Keba"
   },
@@ -31,26 +127,13 @@ let nizOpcija = [
     id: 2,
     vrstaJela: "Sunkarica",
     jedinicaMere: "kg",
-    /* kolicinaJela: {
-      poVojniku: 0.1,
-      svega: 6.9
-    }, */
     kolicinaJelaPoVojniku: 0.1,
     kolicinaJelaSvega: 6.9,
     termickaSredstvaIPosudjeZaPripremuJela: "noz",
     vremeObade: "05.10 - 05.30",
     osobljeZaTermickuObradu: "Radomirovic",
-    /* linija1: {
-      kolicinaJela: "300g",
-      osobljeZaPodelu: "Super Sneki"
-      
-    }, */
     linija1Kolicina: "300g",
     linija1Osoblje: "Ceki",
-    /* linija2: {
-      kolicinaJela: "500g",
-      osobljeZaPodelu: "Mile Kitic"
-    } */
     linija2Kolicina: "500g",
     linija2Osoblje: "Mile"
   },
@@ -58,25 +141,13 @@ let nizOpcija = [
     id: 3,
     vrstaJela: "Sir topljeni",
     jedinicaMere: "kg",
-    /* kolicinaJela: {
-      poVojniku: 0.05,
-      svega: 1.15
-    }, */
     kolicinaJelaPoVojniku: 0.05,
     kolicinaJelaSvega: 1.15,
     termickaSredstvaIPosudjeZaPripremuJela: " / ",
     vremeObade: "05:30 - 05:40",
     osobljeZaTermickuObradu: "Djuric Nenad",
-    /* linija1: {
-      kolicinaJela: "300g",
-      osobljeZaPodelu: "Viki"
-    }, */
     linija1Kolicina: "560g",
     linija1Osoblje: "Viki",
-    /* linija2: {
-      kolicinaJela: "500g",
-      osobljeZaPodelu: "Tuki"
-    } */
     linija2Kolicina: "500g",
     linija2Osoblje: "Tuki"
   },
@@ -84,25 +155,13 @@ let nizOpcija = [
     id: 4,
     vrstaJela: "Jaje kuvano",
     jedinicaMere: "Kom",
-    /* kolicinaJela: {
-      poVojniku: 1,
-      svega: 23
-    }, */
     kolicinaJelaPoVojniku: 1,
     kolicinaJelaSvega: 23,
     termickaSredstvaIPosudjeZaPripremuJela: "1 x 10L",
     vremeObade: "05.30 - 05.50",
     osobljeZaTermickuObradu: "Radomirovic",
-    /* linija1: {
-      kolicinaJela: "300g",
-      osobljeZaPodelu: "Rasta"
-    }, */
     linija1Kolicina: "50g",
     linija1Osoblje: "Sani",
-    /* linija2: {
-      kolicinaJela: "500g",
-      osobljeZaPodelu: "Krasta"
-    } */
     linija2Kolicina: "150g",
     linija2Osoblje: "Baja"
   }
@@ -395,25 +454,24 @@ function insertNewRow() {
 
 function testInsert() {
   // GET TEST TABLE
-  const testTabel = document.getElementsByTagName("table")
+  const testTabel = document.getElementsByClassName("test-table")
 
-  console.log(testTabel);
+  console.log(testTabel)
 
   const newRow = document.getElementsByClassName("tr")
   //console.log(newRow[opcijeObed.value])
-  const newTable = testTabel[opcijeObed.value];
-  console.log(newTable);
+  const newTable = testTabel[opcijeObed.value]
+  console.log(newTable)
   const testRow = newTable.insertRow(-1)
   // SET VALUE FOR LOOP
   //const testCell = testRow.insertCell(-1)
   const vrednost = opcije.value - 1
 
-
   //for (i = 0; i < nizOpcija.length; i++) {
   //console.log(nizOpcija[i])
   //console.log("Vrednost izabranog obeda je --> " + opcijeObed.value)
   console.log(testRow)
-  for (const prop in nizOpcija[vrednost]) {
+  for (const prop in nizOpcijaKrace[vrednost]) {
     if (opcijeObed.value === "dorucak" && opcije.value) {
       //console.log(prop + " ---> " + nizOpcija[i][prop])
       testRow.innerHTML +=
